a4aa926b1fdbe46931b069fd1d54d020f8aa62b1
miawithcode <cuttontail@gmail.com>
2024-01-03 14:05:07 +0800

    feat: fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+
+const url = 'https://course-api.com/react-tabs-project';
+
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+
+  if (isLoading) {
+    return <section>
+      <h2>Loading...</h2>
+    </section>
+  }
+
   return (
     <div>App</div>
   )
